c90653703da68206afc5916e6cb0d7a404ca20e0
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-03-23 16:40:24 +0000

    Initial Commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @Before
+
+    public void before(){
+        calculator= new Calculator(5,11);
+    }
+    @Test
+    public void addNumbers(){
+        assertEquals(16,calculator.add());
+    }
+
+}
