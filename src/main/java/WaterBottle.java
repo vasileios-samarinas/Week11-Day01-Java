3da1d900151a58a75c50c7306330c96059d4bcaf
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-03-23 17:09:22 +0000

    Bottle has volume test pass

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,3 +1,13 @@
 public class WaterBottle {
+ private double volume;
+
+ public WaterBottle(double volume){
+     this.volume=volume;
+ }
+
+ public double getVolume(){
+     return this.volume;
+ }
+
 
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle= new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100,waterBottle.getVolume(),0.01);
+    }
+
 }
